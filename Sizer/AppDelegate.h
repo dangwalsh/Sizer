af1b266e39590475547e1b190ac9002d958612ff
Daniel Walsh <walsh.daniel@icloud.com>
2013-03-23 09:26:32 -0400

    Initial Commit

---
+++ Sizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sizer
+//
+//  Created by Daniel Walsh on 3/23/13.
+//  Copyright (c) 2013 Daniel Walsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sizer
+//
+//  Created by Daniel Walsh on 3/23/13.
+//  Copyright (c) 2013 Daniel Walsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
